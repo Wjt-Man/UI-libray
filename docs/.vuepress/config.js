42faefb81cdc447cefb2fe96ef8b2de07c65a84f
juntong.wu <juntong.wu@ly.com>
2022-01-16 18:03:52 +0800

    feat:finish

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,14 @@
 module.exports={
+    title:"",
     base:"/UI-libray/",
     themeConfig: {
+        nav:[{
+            text:"Home",link:"/"
+        },{
+            text:"Github",link:"https://github.com/Wjt-Man/UI-libray"
+        },{
+            text:"Npm",link:"https://www.npmjs.com/package/hacker-fast-ui"
+        }],
         sidebar: [
             "/",
             "componentsDocs/card"
