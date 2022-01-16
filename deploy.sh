cf4c507ed5a99b782f1ea3b1a1a3e0c94d9f641a
juntong.wu <juntong.wu@ly.com>
2022-01-16 17:33:23 +0800

    卡片组件

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Wjt-Man/test-116.github.io.git master:gh-pages
+git push -f https://github.com/Wjt-Man/hacker-fast-ui.git master:gh-pages
 
 cd -
